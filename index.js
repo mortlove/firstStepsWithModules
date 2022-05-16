21fa08649659ce8cc708ef1cabc5275d63d1ecfb
Alex Gorki <Jojo@jojo.plgit config --global user.emaillscd C:\html css js python\GitHub\Pierwsze kroczkicd 'C:\html css js python\GitHub\Pierwsze kroczki'lslsgit statusgit config --global user.name Aleksandergit config --global user.namegit config --global user.name Aleksander>
2022-05-16 21:33:27 +0000

    Add files: index.js package.json package-lock.json calc.js // module

---
+++ index.js
@@ -0,0 +1,5 @@
+// const { dodawanie, odejmowanie, dzielenie } = require('./src/calc');
+const math = require('./src/calc');
+
+// console.log(dodawanie(2,2))
+console.log(math.dodawanie(2, 2));
---
+++ src/calc.js
@@ -0,0 +1,17 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function substraction(a, b) {
+  return a - b;
+}
+
+module.exports = {
+  dodawanie: sum,
+  dzielenie: divide,
+  odejmowanie: substraction,
+};
